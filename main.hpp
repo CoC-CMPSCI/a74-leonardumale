3c76228c1818f3c8d241a35c57bc3a821e425b9c
Kyu Lee <kwooler@gmail.com>
2026-05-11 00:19:37 -0700

    Strip leaked solution from main.hpp (instructor fix)

--- main.hpp
+++ main.hpp
@@ -14,34 +14,11 @@ void printout(int[], int);
 void insertone(int number[], int &length, int usernum)
 {
     // TODO: insert usernum into the sorted array, then increment length
-    int pos, i;
-    for (i = 0; i < length; i++)
-        if (number[i] > usernum)
-            break;
-    pos = i;
-    for (i = length - 1; i >= pos; i--)
-        number[i + 1] = number[i];
-    number[pos] = usernum;
-    length += 1;
 }
 
 void deleteone(int number[], int &length, int usernum)
 {
     // TODO: find usernum, shift remaining elements left, decrement length
-    int pos = length, i;
-    for (i = 0; i < length; i++)
-    {
-        if (number[i] == usernum)
-        {
-            pos = i;
-            break;
-        }
-    }
-    if (pos == length) return;  // not found
-    for (i = pos; i < length - 1; i++)
-        number[i] = number[i + 1];
-    number[length - 1] = 0;
-    length -= 1;
 }
 
 void printout(int number[], int last)
